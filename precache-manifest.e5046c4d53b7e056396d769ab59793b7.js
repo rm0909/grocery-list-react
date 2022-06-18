d476ef521b7559da234ca6b195e8ed5c0c4a1754
Raphael Machado <raphael.mm.91@gmail.com>
2022-06-18 15:24:42 -0300

    Updates

---
+++ precache-manifest.e5046c4d53b7e056396d769ab59793b7.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "35c138d3e4167e7ac7a0",
+    "url": "/grocery-list-react/static/css/main.7957b9e6.chunk.css"
+  },
+  {
+    "revision": "35c138d3e4167e7ac7a0",
+    "url": "/grocery-list-react/static/js/main.35c138d3.chunk.js"
+  },
+  {
+    "revision": "68915521882ddc9bc3eb",
+    "url": "/grocery-list-react/static/js/1.68915521.chunk.js"
+  },
+  {
+    "revision": "ba0692d8991d39308aff",
+    "url": "/grocery-list-react/static/js/runtime~main.ba0692d8.js"
+  },
+  {
+    "revision": "7cd8e502e143f611521fef1898ef4e74",
+    "url": "/grocery-list-react/index.html"
+  }
+];
